5ebe6e15ec0c934d05e6d7f2ceb9b8dd6a19fc5e
Hafiz Waleed Hussain <hafizwaleedhussain@gmail.com>
2019-07-28 13:38:10 +0700

    First commit

---
+++ app/src/main/java/com/uwantolearn/loladapter/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.uwantolearn.loladapter
+
+import android.opengl.Visibility
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
